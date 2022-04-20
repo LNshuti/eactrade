62b210ad72b6a1205c72dfb303b240d4b39f8f12
LNshuti <nshutl0@sewanee.edu>
2022-04-19 22:34:10 -0500

    initial commit

---
+++ R/manifest.R
@@ -0,0 +1,56 @@
+# Load necessary libraries and functions
+library(finnts)
+library(data.table)
+library(igraph)
+library(ggraph)
+library(igraphdata)
+library(tidygraph)
+library(gt)
+library(tidyverse)
+
+theme_tufte_revised <- function(base_size = 11, base_family = "Gill Sans", ticks = TRUE) {
+  
+  ret <- ggplot2::theme_bw(base_family = base_family, base_size = base_size) +
+    ggplot2::theme(
+      axis.line = ggplot2::element_line(color = 'black'),
+      axis.title.x = ggplot2::element_text(vjust = -0.3),
+      axis.title.y = ggplot2::element_text(vjust = 0.8),
+      legend.background = ggplot2::element_blank(),
+      legend.key = ggplot2::element_blank(),
+      legend.title = ggplot2::element_text(face="plain"),
+      panel.background = ggplot2::element_blank(),
+      panel.border = ggplot2::element_blank(),
+      panel.grid = ggplot2::element_blank(),
+      plot.background = ggplot2::element_blank(),
+      strip.background = ggplot2::element_blank()
+    )
+  
+  if (!ticks) {
+    ret <- ret + ggplot2::theme(axis.ticks = ggplot2::element_blank())
+  }
+  
+  ret
+}
+
+
+my_theme <- 
+  theme_classic(base_family = "Times") +
+  theme(axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.border = ggplot2::element_blank(),
+        panel.grid = ggplot2::element_blank(),
+        axis.line.y = element_blank(), 
+        legend.background = element_rect()
+  ) 
+
+
+# Create function to convert dataframe to bipartite matrix
+convert_to_bipartite <- function(df,id) {
+  id <- enquo(id)
+  nn <- df %>% pull(!!id)
+  foo <- df %>% select(-!!id) %>%
+    as.matrix()
+  
+  rownames(foo) <- nn
+  foo
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## Using Network Analysis to Understand International trade
+
+#### Definitions
+- **A random graph** has two parameters: *N* and *P* where *N* represents the number of nodes, and *P* the probability distribution that connects a given set of nodes.
+
+- **A node** is a vertex of a graph.
+
+- **An edge** is a connection or link between **nodes**.
+
+
+#### **References**
+- Sadler, Jesse. Network Analysis with R. https://www.jessesadler.com/post/network-analysis-with-r/
+- Katherine Ognyanova. Network analysis with R and igraph: NetSci X Tutorial.
+  https://kateto.net/networks-r-igraph
+- Matthew Kay (2021). tidybayes: Tidy Data and Geoms for Bayesian Models. R package version 3.0.2, https://mjskay.github.io/tidybayes/. DOI: 10.5281/zenodo.1308151.
