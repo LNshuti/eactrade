35f93a21d68d66d242028ae3f8b7930c413e0ac7
LNshuti <nshutl0@sewanee.edu>
2022-04-20 13:55:41 -0500

    update readme

--- R/01_data_aggregation_task.R
+++ R/01_data_aggregation_task.R
@@ -1,10 +1,10 @@
-repo_ <- "rwandatrade"
+repo_ <- "eactrade"
 source(paste0(repo_, "/R/manifest.R"))
 
 
 #select products of interest(sitc_product_code == "transportation")
 product2digit <-
- read_csv("data/country_sitcproduct2digit_year.csv")
+ read_csv(paste0(repo_, "/data/dataverse_files/country_sitcproduct2digit_year.csv"))
 
 #Import country identifiers
 countryweb <-
@@ -21,18 +21,18 @@ country_codes <-
 
 #Combine trade data from 2015 to 2019
 trade_data_all_years <-
- list.files("data/dataverse_files/", recursive = TRUE) %>%
+ list.files(paste0(repo_, "/data/dataverse_files"), recursive = TRUE) %>%
  as_tibble() %>%
  filter(grepl(pattern = "partner_sitcproduct4digit", x = value)) %>%
  pull(value)
 
-rwanda_trade_df <-
+eac_trade_df <-
  trade_data_all_years %>%
- map(~data.table::fread(file = paste0("data/dataverse_files/",.x)) %>%
+ map(~data.table::fread(file = paste0(repo_, "/data/dataverse_files/",.x)) %>%
        janitor::clean_names() %>% as_tibble() %>%
        mutate_all(as.character) %>%
        filter(location_code %in% c("RWA","UGA","TZA","BDI", "COD", "KEN"))
      ) %>%
- bind_rows() %>% as_tibble() %>% filter(year > 2014)
+ bind_rows() %>% as_tibble() 
 
-saveRDS(object = rwanda_trade_df, "data/processed/eac_trade_df_2015_2019.rds")
+saveRDS(object = eac_trade_df, "eactrade/data/processed/eac_trade_df_2015_2019.rds")
