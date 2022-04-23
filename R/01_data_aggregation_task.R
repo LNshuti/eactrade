26c9ce3817c2805e139c82507e01f37e48a2cb53
LNshuti <nshutl0@sewanee.edu>
2022-04-23 15:55:34 -0500

    update processing step

--- R/01_data_aggregation_task.R
+++ R/01_data_aggregation_task.R
@@ -1,44 +1,34 @@
 repo_ <- "eactrade"
 source(paste0(repo_, "/R/manifest.R"))
 
-
-# select products of interest(sitc_product_code == "transportation")
-# 
-product2digit <-
-  data.table::fread(paste0(repo_,
-                           "/data/dataverse_files/country_sitcproduct2digit_year.csv")) %>% 
-  as_tibble() %>%
-  select(partner_code, location_code) %>%
-  unique()
-
-#Import country identifiers
-countryweb <-
- "https://pkgstore.datahub.io/JohnSnowLabs/country-and-continent-codes-list/country-and-continent-codes-list-csv_csv/data/b7876b7f496677669644f3d1069d3121/country-and-continent-codes-list-csv_csv.csv"
-
-#Get EAC country codes
-country_codes <-
- read.csv(countryweb) %>%
- janitor::clean_names() %>%
- select(continent_code,
-        country_code = three_letter_country_code) %>%
- filter(country_code %in% c("RWA","UGA","TZA","BDI", "COD", "KEN")) %>%
- rename(continent_name =continent_code)
+country_ids <- 
+  read_csv("eactrade/data/countries_codes_and_coordinates.csv") %>%
+  janitor::clean_names() %>%
+  select(alpha_3_code, numeric_code) %>%
+  filter(alpha_3_code %in% c("RWA","UGA","TZA","BDI", "COD", "KEN"))  %>%
+  mutate(numeric_code = as.character(numeric_code))
 
 #Combine trade data from 2015 to 2019
 trade_data_all_years <-
- list.files(paste0(repo_, "/data/dataverse_files"), recursive = TRUE) %>%
+ list.files(paste0("dataverse_files"), recursive = TRUE) %>%
  as_tibble() %>%
  filter(grepl(pattern = "partner_sitcproduct4digit", x = value)) %>%
  pull(value)
 
-eac_trade_df <-
+allcountries_trade_df <-
  trade_data_all_years %>%
- map(~data.table::fread(file = paste0(repo_, "/data/dataverse_files/",.x)) %>%
+ map(~data.table::fread(file = paste0("dataverse_files/",.x)) %>%
        janitor::clean_names() %>% as_tibble() %>%
-       mutate_all(as.character) %>%
-       filter(location_code %in% c("RWA","UGA","TZA","BDI", "COD", "KEN") &
-              partner_code %in% c("RWA","UGA","TZA","BDI", "COD", "KEN"))
+       mutate_all(as.character)
      ) %>%
  bind_rows() %>% as_tibble() 
+
+
+eaccountries_trade_df <- 
+  allcountries_trade_df %<%
+  inner_join(country_ids, by = c("location_id" = "numeric_code")) %>%
+  rename(to = alpha_3_code) %>%
+  inner_join(country_ids, by = c("partner_id" = "numeric_code")) %>%
+  rename(from = alpha_3_code) 
 
 saveRDS(object = eac_trade_df, "eactrade/data/processed/eac_trade_df_2015_2019.rds")
