1d215c4c2d8e39e8ee207eb8b6137196a762b063
Leonce Nshuti <LNshuti@users.noreply.github.com>
2023-02-19 10:37:17 -0600

    update readme

--- python/explore_energy_generation.py
+++ python/explore_energy_generation.py
@@ -10,19 +10,17 @@ import locale
 
 # Read in the data
 product_labs = pd.read_csv('../data/processed/SITCCodeandDescription.csv')
-
-# Read in the data
 trade_data_all_years = pq.ParquetDataset('../data/processed/country_partner_sitcproduct4digit_year_all.parquet').read_pandas().to_pandas()
+
 print(trade_data_all_years.head(10))
+
+# COnvert numeric columns to numeric data type
 trade_data_all_years['export_value'] = pd.to_numeric(trade_data_all_years['export_value'], errors='coerce')
 trade_data_all_years['import_value'] = pd.to_numeric(trade_data_all_years['import_value'], errors='coerce')
 trade_data_all_years['trade_balance'] = trade_data_all_years['export_value'] - trade_data_all_years['import_value']
 
+# Merge the product labels to the trade data
 labelled_df = trade_data_all_years.merge(product_labs, left_on='sitc_product_code', right_on='parent_code', how='inner')
-
-#print(labelled_df.head(10))
-# Filter location_code to the USA, CHN, and RUS (China, Russia, and the United States)
-#labelled_df = labelled_df[labelled_df['location_code'].isin(['RWA', 'BDI', 'UGA'])]
 
 # Summarize the trade_balance by location_code and product_description
 labelled_df = labelled_df.groupby(['year','location_code', 'parent_code', 'partner_code', 'description'])['trade_balance'].sum().reset_index()
@@ -30,17 +28,20 @@ labelled_df = labelled_df.groupby(['year','location_code', 'parent_code', 'partn
 # Create a new variable trade_balance_millions to make the numbers more readable
 labelled_df['trade_balance_millions'] = labelled_df['trade_balance'] / 1000000
 
-# Filter to the top 10 products by trade balance for CHN 
-rwa_df = labelled_df[labelled_df['location_code'] == 'RWA'].sort_values(by='trade_balance_millions', ascending=False)
-uga_df = labelled_df[labelled_df['location_code'] == 'UGA'].sort_values(by='trade_balance_millions', ascending=False)
+# Create a function that returns the top 10 products by trade balance for a given location_code
+def top10_products(df, location_code):
+    # Filter to the top 10 products by trade balance for CHN 
+    top10 = df[df['location_code'] == location_code].sort_values(by='trade_balance_millions', ascending=False).head(10)
+    # Select unique values from the parent_code and description columns 
+    top10 = top10[['year','parent_code','location_code', 'partner_code', 'description', 'trade_balance_millions']].drop_duplicates()
+    # convert to polars dataframe
+    top10 = pl.from_pandas(top10)
+    return top10
 
-# Select unique values from the parent_code and description columns 
-rwa_df = rwa_df[['year','parent_code','location_code', 'partner_code', 'description', 'trade_balance_millions']].drop_duplicates()
-uga_df = uga_df[['year','parent_code','location_code', 'partner_code', 'description', 'trade_balance_millions']].drop_duplicates()
-# convert to polars dataframe
-rwa_df = pl.from_pandas(rwa_df)
-uga_df = pl.from_pandas(uga_df)
-#print(rwa_df)
+rwa_df = top10_products(labelled_df, 'RWA')
+uga_df = top10_products(labelled_df, 'UGA')
+ken_df = top10_products(labelled_df, 'KEN')
+bdi_df = top10_products(labelled_df, 'BDI')
 
 # Select the top 10 partner_code by trade balance for RWA
 rwa_top10 = rwa_df.groupby('partner_code').agg(pl.sum('trade_balance_millions')).sort('trade_balance_millions', reverse=True).head(10)
@@ -67,6 +68,8 @@ def plot_top10_partners(df, location_code):
 # Call the function
 plot_top10_partners(rwa_df, 'RWA')
 plot_top10_partners(uga_df, 'UGA')
+plot_top10_partners(ken_df, 'KEN')
+plot_top10_partners(bdi_df, 'BDI')
 # Plot bar plot andsave plot as png to output folder. Use seaborn for styling
 # fig, ax = plt.subplots(figsize=(10, 6))
 # sns.set_style("whitegrid")
