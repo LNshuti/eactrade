2aafd7cd93d66372677f83423031622d257fb8ee
Leonce <nshutl0@sewanee.edu>
2023-02-23 13:20:11 -0600

    add all africa plots

---
+++ python/all_africa_debt_income_demogs.py
@@ -0,0 +1,105 @@
+import pyarrow.parquet as pq
+import polars as pl
+import pandas as pd
+import squarify
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Read in the data
+product_labs = pd.read_csv('../data/processed/SITCCodeandDescription.csv')
+trade_data_all_years = pq.ParquetDataset('../data/processed/country_partner_sitcproduct4digit_year_all.parquet').read_pandas().to_pandas()
+# Import the population data
+# It is in csv format. Skip first four rows 
+# Fifth row contains the column names
+# Write the code 
+pop_data = pd.read_csv('../data/processed/API_SP_POP_TOTL_DS2.csv', skiprows=4)
+
+#print(trade_data_all_years.head(10))
+
+# COnvert numeric columns to numeric data type
+trade_data_all_years['export_value'] = pd.to_numeric(trade_data_all_years['export_value'], errors='coerce')
+trade_data_all_years['import_value'] = pd.to_numeric(trade_data_all_years['import_value'], errors='coerce')
+trade_data_all_years['trade_balance'] = trade_data_all_years['export_value'] - trade_data_all_years['import_value']
+
+# Merge the product labels to the trade data
+labelled_df = trade_data_all_years.merge(product_labs, left_on='sitc_product_code', right_on='parent_code', how='inner')
+
+# Summarize the trade_balance by location_code and product_description
+labelled_df = labelled_df.groupby(['year','location_code', 'parent_code', 'partner_code', 'description'])['trade_balance'].sum().reset_index()
+
+# Merge the population data to the trade data
+# Only keep the population data for 2020 
+# Write the code
+pop_data = pop_data[['Country Code', 'Country Name', '2020']]
+
+# Rename 2020 to pop_2020
+pop_data = pop_data.rename(columns={'2020': 'pop_2020'})
+
+# Create a new variable trade_balance_millions to make the numbers more readable
+labelled_df['trade_balance_millions'] = labelled_df['trade_balance'] / 1000000
+
+# Join pop_data 
+labelled_df = labelled_df.merge(pop_data, left_on='location_code', right_on='Country Code', how='inner')
+
+# Create a trade_bal_by_population variable. This variable is the trade balance divided by the population 
+# Write the code
+labelled_df['trade_bal_by_population'] = labelled_df['trade_balance'] / labelled_df['pop_2020']
+
+# Create a function that returns the top 10 products by trade balance for a given location_code
+def top10_products(df, location_code):
+    # Filter to the top 10 products by trade balance for CHN 
+    top10 = df[df['location_code'] == location_code].sort_values(by='trade_balance_millions', ascending=False).head(10)
+    # Select unique values from the parent_code and description columns 
+    top10 = top10[['year','parent_code','location_code', 'partner_code', 'description', 'trade_balance_millions', 'trade_balance', 'pop_2020', 'trade_bal_by_population']].drop_duplicates()
+    # convert to polars dataframe
+    top10 = pl.from_pandas(top10)
+    return top10
+
+rwa_df = top10_products(labelled_df, 'RWA')
+uga_df = top10_products(labelled_df, 'UGA')
+ken_df = top10_products(labelled_df, 'KEN')
+bdi_df = top10_products(labelled_df, 'BDI')
+tza_df = top10_products(labelled_df, 'TZA')
+
+### SADEC 
+bwa_df = top10_products(labelled_df, 'BWA')
+moz_df = top10_products(labelled_df, 'MOZ')
+zmb_df = top10_products(labelled_df, 'ZMB')
+ago_df = top10_products(labelled_df, 'AGO')
+mwi_df = top10_products(labelled_df, 'MWI')
+lso_df = top10_products(labelled_df, 'LSO')
+swz_df = top10_products(labelled_df, 'SWZ')
+nam_df = top10_products(labelled_df, 'NAM')
+
+### ECOWAS 
+gha_df = top10_products(labelled_df, 'GHA')
+nga_df = top10_products(labelled_df, 'NGA')
+sen_df = top10_products(labelled_df, 'SEN')
+bfa_df = top10_products(labelled_df, 'BFA')
+ben_df = top10_products(labelled_df, 'BEN')
+gmb_df = top10_products(labelled_df, 'GMB')
+tgo_df = top10_products(labelled_df, 'TGO')
+civ_df = top10_products(labelled_df, 'CIV')
+gnb_df = top10_products(labelled_df, 'GNB')
+lbr_df = top10_products(labelled_df, 'LBR')
+
+
+sadec_df = pl.concat([bwa_df, moz_df, zmb_df, ago_df, mwi_df, tza_df, lso_df, swz_df, nam_df])
+aggregated_sadecc_df = (
+    sadec_df
+    .groupby(['location_code'])
+    .agg(
+        pl.col('trade_bal_by_population').mean().alias('avg_trade_bal_per_capita')
+    ) )
+
+print(aggregated_sadecc_df)
+
+# Convert polars table to png and save to output 
+# Write the code
+fig, ax = plt.subplots(figsize=(5, 3))
+sns.set_style("whitegrid")
+sns.factorplot(x='avg_trade_bal_per_capita', y='location_code', data=aggregated_sadecc_df.to_pandas(), kind='bar')
+plt.title('Trade balance per capita in USD')
+plt.xlabel('USD')
+plt.ylabel('Country')
+plt.savefig('../output/avg_trade_bal_per_capita_sadec.png', dpi=300, bbox_inches='tight')
